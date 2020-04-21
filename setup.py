2394460bb38b458e110083f3215bf805010043ee
sheepmanchen <sheepman314@gmail.com>
2020-04-21 23:11:28 +0800

    modified requirements

--- setup.py
+++ setup.py
@@ -51,7 +51,7 @@ setup(
     setup_requires=setup_requirements,
     test_suite='tests',
     tests_require=test_requirements,
-    # url='https://github.com/aronwc/elevate_osna',
+    url='https://github.com/sheepmanchen/Twitter-Bots-Detector',
     version='0.1.0',
     zip_safe=False,
 )
